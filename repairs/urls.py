5da0b0c66a05bcd2a91be991fec2b0288dd2a400
NjiaMauzo-Afrika <161914591+Njiafilm@users.noreply.github.com>
2026-09-08 06:06:23 +0300

    Refactor URL patterns in repairs/urls.py

--- repairs/urls.py
+++ repairs/urls.py
@@ -1,3 +1,9 @@
-path('matengenezo/', views.matengenezo_page, name='matengenezo'),
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.matengenezo_page, name='matengenezo_home'),
+    path('matengenezo/', views.matengenezo_page, name='matengenezo'),
     path('api/categories/', views.api_categories, name='api_categories'),
     path('api/guides/', views.api_guides, name='api_guides'),
+]
