5c5d20eff5c189b847a9511a0bcefd6da8749811
NjiaMauzo-Afrika <161914591+Njiafilm@users.noreply.github.com>
2026-09-09 18:42:05 +0300

    Update urls.py

--- repairs/urls.py
+++ repairs/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.matengenezo_page, name='matengenezo_home'),
     path('matengenezo/', views.matengenezo_page, name='matengenezo'),
+    path('ongeza-guide/', views.ongeza_repair_guide, name='ongeza_repair_guide'),  # Njia mpya ya fomu ya kuongeza guide
     path('api/categories/', views.api_categories, name='api_categories'),
     path('api/guides/', views.api_guides, name='api_guides'),
 ]
