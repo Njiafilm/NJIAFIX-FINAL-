7b32620cfb33029ca3b35014100da06cbca6994a
NjiaMauzo-Afrika <161914591+Njiafilm@users.noreply.github.com>
2026-09-12 06:42:17 +0300

    Add files via upload

---
+++ NjiafixApplication.java
@@ -0,0 +1,12 @@
+package com.njiafix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjiafixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NjiafixApplication.class, args);
+    }
+}
