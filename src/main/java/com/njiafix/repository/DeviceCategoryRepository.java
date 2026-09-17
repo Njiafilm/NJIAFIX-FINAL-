a84e9fe448bce2052546f5b74cc3404f5555b169
NjiaMauzo-Afrika <161914591+Njiafilm@users.noreply.github.com>
2026-09-17 10:36:00 +0300

    Delete src/main directory

--- src/main/java/com/njiafix/repository/DeviceCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.njiafix.repository;
-
-import com.njiafix.model.DeviceCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeviceCategoryRepository extends JpaRepository<DeviceCategory, Long> {
-}
